2323c246d6b5d02366ec8ee54f387c3527ea123f
Sam O Reilly <x23756575@student.ncirl.ie>
2025-06-11 16:32:46 +0100

    controllers and services setup to take in url via postrequest and then send to kafka

--- src/main/java/debate/debate/controllers/UrlCon.java
+++ src/main/java/debate/debate/controllers/UrlCon.java
@@ -1,4 +1,25 @@
 package debate.debate.controllers;
 
+import debate.debate.services.UrlService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
 public class UrlCon {
+
+    private final UrlService urlService;
+
+    public UrlCon(UrlService urlService){
+        this.urlService = urlService;
+    }
+
+    @PostMapping("/url")
+    public ResponseEntity<String> receiveURl(String url){
+        urlService.getUrl(url);
+        return ResponseEntity.ok("Url sent to kafka");
+    }
 }
---
+++ src/main/java/debate/debate/services/UrlService.java
@@ -0,0 +1,21 @@
+package debate.debate.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public UrlService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void getUrl(String url){
+
+       kafkaTemplate.send("url",url);
+
+    }
+}
