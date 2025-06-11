4b5a483f3c7d2324a48cb6528c0478e8096ccf41
Sam O Reilly <x23756575@student.ncirl.ie>
2025-06-11 15:05:56 +0100

    setup

---
+++ src/main/java/debate/debate/DebateAnalyserApplication.java
@@ -0,0 +1,13 @@
+package debate.debate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebateAnalyserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebateAnalyserApplication.class, args);
+    }
+
+}
